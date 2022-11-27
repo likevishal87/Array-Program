15ffdb1a9f998f78fc77c3025b99ce9e35ec3c56
Vicky Verma <likevishalverma@gmail.com>
2022-11-27 19:46:07 +0530

    The largest elements in an array

---
+++ LargestArray.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.day3;
+
+//Java Program to print the largest element in an array
+public class LargestArray {
+
+	public static void main(String[] args) {
+		int[] arr = new int[] { 25, 11, 7, 75, 56 };
+		int max = arr[0];
+		// Loop through the array
+		for (int i = 0; i < arr.length; i++) {
+			// Compare elements of array with max
+			if (arr[i] > max)
+				max = arr[i];
+		}
+		System.out.println("Largest element present in given array: " + max);
+	}
+
+}
